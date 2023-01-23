a55285c5f788770704335ac5468623be3387525c
Lirak Haxhikadrija <lirak.haxhikadrijawork@gmail.com>
2023-01-23 18:57:47 +0100

    Move image to src folder under assets

--- src/layouts/Navbar/Navbar.js
+++ src/layouts/Navbar/Navbar.js
@@ -1,51 +1,53 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
+import logo from "../../assets/starwars.png";
+
 import "./Navbar.css";
 
 function Navbar() {
-  return (
-    <div className="Navbar">
-      <div className="Navbar_main">
-        <img src="starwars.png" alt="logo" width="350px" height="150px" />
-      </div>
-      <nav className="Navbar_nav">
-        <NavLink
-          className={({ isActive }) =>
-            `Navbar_link ${isActive && "Navbar_link_active"}`
-          }
-          to="/"
-        >
-          Home
-        </NavLink>
+	return (
+		<div className="Navbar">
+			<div className="Navbar_main">
+				<img src={logo} alt="logo" width="350px" height="150px" />
+			</div>
+			<nav className="Navbar_nav">
+				<NavLink
+					className={({ isActive }) =>
+						`Navbar_link ${isActive && "Navbar_link_active"}`
+					}
+					to="/"
+				>
+					Home
+				</NavLink>
 
-        <NavLink
-          className={({ isActive }) =>
-            `Navbar_link ${isActive && "Navbar_link_active"}`
-          }
-          to="/starships"
-        >
-          Starships
-        </NavLink>
-        <NavLink
-          className={({ isActive }) =>
-            `Navbar_link ${isActive && "Navbar_link_active"}`
-          }
-          to="/character"
-        >
-          Character
-        </NavLink>
-        <NavLink
-          className={({ isActive }) =>
-            `Navbar_link ${isActive && "Navbar_link_active"}`
-          }
-          to="/planets"
-        >
-          Planets
-        </NavLink>
-      </nav>
-    </div>
-  );
+				<NavLink
+					className={({ isActive }) =>
+						`Navbar_link ${isActive && "Navbar_link_active"}`
+					}
+					to="/starships"
+				>
+					Starships
+				</NavLink>
+				<NavLink
+					className={({ isActive }) =>
+						`Navbar_link ${isActive && "Navbar_link_active"}`
+					}
+					to="/character"
+				>
+					Character
+				</NavLink>
+				<NavLink
+					className={({ isActive }) =>
+						`Navbar_link ${isActive && "Navbar_link_active"}`
+					}
+					to="/planets"
+				>
+					Planets
+				</NavLink>
+			</nav>
+		</div>
+	);
 }
 
 export default Navbar;
